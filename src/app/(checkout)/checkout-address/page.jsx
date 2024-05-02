3df7fe0efd4cf0248126ca7af30bb129439b0d1f
GOBORAM <109337202+g-boram@users.noreply.github.com>
2024-05-02 15:03:09 +0900

    토스 결제 기능구현

--- src/app/(checkout)/checkout-address/page.jsx
+++ src/app/(checkout)/checkout-address/page.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import CheckoutAddressClient from './CheckoutAddressClient'
 
 // 물건 주문시 배송주소, 배달을 시키는 사람의 주소를 넣어주는 컴포넌트
 const CheckoutAddress = () => {
   return (
-    <div>
-      CheckoutAddress
-    </div>
+    <CheckoutAddressClient />
   )
 }
 
